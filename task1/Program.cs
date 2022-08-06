79a9b291b81737ca89b27b4a0ac3eaea783824ec
Alex <newalexis777@gmail.com>
2022-08-06 20:54:33 +0400

    task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите 1 число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int y = int.Parse(Console.ReadLine());
+int c = y * y;
+
+if (x == c)
+{
+    Console.Write(x);
+    Console.Write(" является квадратом числа ");
+    Console.Write(y);
+}
+else
+{
+    Console.Write(x);
+    Console.Write(" не является квадратом числа ");
+    Console.Write(y);
+}
