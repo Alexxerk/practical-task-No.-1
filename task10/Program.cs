8832a8d40f08e86cd7d62fb5c3030a8ce2c682a2
Alex <newalexis777@gmail.com>
2022-08-07 19:26:37 +0400

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите делимое число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите делитель: ");
+int y = int.Parse(Console.ReadLine());
+int a = 0;
+
+if (x % y == 0)
+{
+    Console.Write(y);
+    Console.Write(" является делителем ");
+    Console.WriteLine(x);
+    a = x / y;
+    Console.Write(a);
+    Console.Write(" ответ");
+}
+else
+{
+    Console.Write(y);
+    Console.Write(" не является делителем ");
+    Console.WriteLine(x);
+    a = x % y;
+    Console.Write(a);
+    Console.Write(" является остатком от деления ");
+}
