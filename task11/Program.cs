1239fbd451ead6ed16e2bb26aaaa1a08fd8e503e
Alex <newalexis777@gmail.com>
2022-08-07 20:31:37 +0400

    task 11

---
+++ task11/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int y = 10;
+int count = 0;
+int z = 0;
+
+while(x / y != 0)
+{
+    y = y * 10;
+    count = count + 1;
+}
+
+if (count < 2)
+{
+    Console.Write("Число меньше трехзначного ");
+}
+else
+{
+
+z = 3 - count;
+z = -z + 1;
+
+ while(z != 0)
+ {
+     x = x / 10;
+     z = z - 1;
+ }
+
+ z = x % 10;
+ Console.Write("Третья цифра числа == ");
+ Console.Write(z);
+}
+
+
+
+
