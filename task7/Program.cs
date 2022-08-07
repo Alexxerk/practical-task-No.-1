a740c3d54355058d02b6954ad95ec3798ccbdb73
Alex <newalexis777@gmail.com>
2022-08-07 16:10:39 +0400

    task 7

---
+++ task7/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите трехзначное число: ");
+int x = int.Parse(Console.ReadLine());
+int y = 0;
+
+if (x < -99 && x >-1000)
+{
+    Console.Write("Вывожу вторую цифру введенного числа: ");
+    y = x / 10;
+    y = y % 10;
+    Console.Write(-y);
+}
+else if (x > 99 && x <1000)
+{
+    Console.Write("Вывожу вторую цифру введенного числа: ");
+    y = x / 10;
+    y = y % 10;
+    Console.Write(y);
+}
+else
+{
+    Console.Write("Вы ввели некоректное число ");
+}
