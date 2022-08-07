935e2d9d097e62f4026c0e0227da4a275313fc8a
Alex <newalexis777@gmail.com>
2022-08-07 10:55:35 +0400

    task 5

---
+++ task5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+int x = (-n - 1);
+
+while(x < (n - 1))
+{
+    x = x + 1;
+    Console.Write(x);
+    Console.Write(", ");
+}
+Console.Write(n);
