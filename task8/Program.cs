8e65d143a920a4f5b453e4a24e48a1f862e0bd91
Alex <newalexis777@gmail.com>
2022-08-07 18:25:12 +0400

    task 8

---
+++ task8/Program.cs
@@ -0,0 +1,27 @@
+﻿int x = new Random().Next(10, 100);
+Console.Write("Число ");
+Console.WriteLine(x);
+
+int max = 0;
+
+int y = x / 10;
+int z = x % 10;
+
+if (y > z)
+{
+    max = y;
+    Console.WriteLine("Наибольшее из двух цифр числа == ");
+    Console.Write(max);
+}
+else if (y < z)
+{
+    max = z;
+    Console.WriteLine("Наибольшее из двух цифр числа == ");
+    Console.Write(max);
+}
+else 
+{
+    Console.WriteLine("Цифры входящие в число равны ");
+}
+
+
