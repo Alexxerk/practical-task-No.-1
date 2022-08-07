49696d070783377023836c88df599dc40dcea9dd
Alex <newalexis777@gmail.com>
2022-08-07 10:27:58 +0400

    task 4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.Write("Данное число является четным. ");
+}
+else
+{
+   Console.Write("Данное число является нечетным. "); 
+}
